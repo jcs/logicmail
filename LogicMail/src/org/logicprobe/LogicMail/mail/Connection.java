05a2a7ac167cdfdde9cc12a21b600ed99076a92e
octorian <octorian@5c734088-3d25-0410-9155-b3c3832efda5>
2006-08-02 02:11:57 +0000

    Licence banner additions
    git-svn-id: https://logicmail.svn.sourceforge.net/svnroot/logicmail/trunk@15 5c734088-3d25-0410-9155-b3c3832efda5

--- LogicMail/src/org/logicprobe/LogicMail/mail/Connection.java
+++ LogicMail/src/org/logicprobe/LogicMail/mail/Connection.java
@@ -1,8 +1,41 @@
+/*-
+ * Copyright (c) 2006, Derek Konigsberg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution. 
+ * 3. Neither the name of the project nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /******************************************************************************
  * This class has been modified to integrate J2ME socket connection code,
  * since LogicMail has no need for a dual-use implementation.  The bulk of
  * this code is a combination of Connection.java and the J2ME ConnectionImpl.java
- * from Mail4ME, and is covered under the copyright shown below:
+ * from Mail4ME, and is covered under the copyright shown below.  Any code not
+ * originating from Mail4ME is covered under the copyright shown at the top of
+ * this source file.
  * 
  * Mail4ME - Mail for the Java 2 Micro Edition
  *
