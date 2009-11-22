be8822a83f200c80e81ec32e3de94c9c0de4b657
octorian <octorian@5c734088-3d25-0410-9155-b3c3832efda5>
2009-11-22 20:28:10 +0000

    git-svn-id: https://logicmail.svn.sourceforge.net/svnroot/logicmail/trunk@554 5c734088-3d25-0410-9155-b3c3832efda5

--- LogicMail/src/org/logicprobe/LogicMail/LogicMail.java
+++ LogicMail/src/org/logicprobe/LogicMail/LogicMail.java
@@ -120,13 +120,6 @@ public class LogicMail extends UiApplication {
                     // Initialize the navigation controller
                     navigationController = new NavigationController(LogicMail.this);
 
-                    try {
-                        sleep(2000);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                    
                     invokeLater(new Runnable() {
                         public void run() {
                             // Push the mail home screen and pop
