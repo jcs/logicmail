02cfa97bc97007941af08bdcebea45647aad2fc7
octorian <octorian@5c734088-3d25-0410-9155-b3c3832efda5>
2010-07-23 01:18:57 +0000

    Rewrite of network connection receive() code, and corresponding removal of the last vestiges of Mail4ME from the LogicMail source tree.
    git-svn-id: https://logicmail.svn.sourceforge.net/svnroot/logicmail/trunk@679 5c734088-3d25-0410-9155-b3c3832efda5

--- LogicMail/src/org/logicprobe/LogicMail/util/Connection.java
+++ LogicMail/src/org/logicprobe/LogicMail/util/Connection.java
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2006, Derek Konigsberg
+ * Copyright (c) 2010, Derek Konigsberg
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,40 +29,13 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/******************************************************************************
- * This class has been modified to integrate J2ME socket connection code,
- * since LogicMail has no need for a dual-use implementation.  The bulk of
- * this code is a combination of Connection.java and the J2ME ConnectionImpl.java
- * from Mail4ME, and is covered under the copyright shown below.  Any code not
- * originating from Mail4ME is covered under the copyright shown at the top of
- * this source file.
- *
- * Mail4ME - Mail for the Java 2 Micro Edition
- *
- * A lightweight, J2ME- (and also J2SE-) compatible package for sending and
- * receiving Internet mail messages using the SMTP and POP3 protocols.
- *
- * Copyright (c) 2000-2002 Jorg Pleumann <joerg@pleumann.de>
- *
- * Mail4ME is part of the EnhydraME family of projects. See the following web
- * sites for more information:
- *
- * -> http://mail4me.enhydra.org
- * -> http://me.enhydra.org
- *
- * Mail4ME is distributed under the Enhydra Public License (EPL), which is
- * discussed in great detail here:
- *
- * -> http://www.enhydra.org/software/license/index.html
- *
- * Have fun!
- ******************************************************************************/
 package org.logicprobe.LogicMail.util;
 
 import net.rim.device.api.crypto.tls.tls10.TLS10Connection;
 import net.rim.device.api.i18n.ResourceBundle;
+import net.rim.device.api.io.NoCopyByteArrayOutputStream;
 import net.rim.device.api.system.EventLogger;
-import net.rim.device.api.util.DataBuffer;
+import net.rim.device.api.util.Arrays;
 import net.rim.device.cldc.io.ssl.TLSException;
 
 import org.logicprobe.LogicMail.AppInfo;
@@ -71,6 +44,7 @@ import org.logicprobe.LogicMail.conf.ConnectionConfig;
 import org.logicprobe.LogicMail.conf.GlobalConfig;
 import org.logicprobe.LogicMail.conf.MailSettings;
 
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -82,22 +56,11 @@ import javax.microedition.io.StreamConnection;
 
 
 /**
- * Is the abstract base class for socket connections used inside the SMTP,
- * POP3 and IMAP protocols of the mail package. The rationale for using this class
- * is the difference in how networking is handled in J2ME and J2SE: While J2SE
- * has java.net.Socket, J2ME uses the Generic Connection Framework with its
- * javax.io.Connector class as a central means to open sockets or HTTP
- * connection. Unfortunately, both methods are totally incompatible with each
- * other, so a common abstraction has to be found to make the mail package
- * work in both environments.
- * <p>
- * The Connection class is such an abstraction. It provides abstract
- * versions of methods that open and close sockets, read from them and write
- * to them. It also provides a static factory method that is able to instantiate
- * one of several sub-classes of Connection that match the different run-time
- * environments. These sub-classes are all called ConnectionImpl, and they are
- * loaded "by name" from the j2me, j2se, or http packages, respectively. This
- * is the only way to get rid of compile-time dependencies on these classes.
+ * This is the abstract base class for socket connections used inside the SMTP,
+ * POP3, and IMAP protocols of the mail package.  It handles the details of
+ * sending data in what ever manner is appropriate to the mail protocol, and
+ * receiving data in whole lines.  The specifics of opening network connections
+ * on the BlackBerry platform is delegated to subclasses.
  */
 public abstract class Connection {
     protected static ResourceBundle resources = ResourceBundle.getBundle(LogicMailResource.BUNDLE_ID, LogicMailResource.BUNDLE_NAME);
@@ -113,10 +76,10 @@ public abstract class Connection {
     /** Select WAP 2.0 */
     protected static final int TRANSPORT_WAP2 = 0x08;
 
-    /**
-     * Byte array holding carriage return and line feed
-     */
-    private static final byte[] CRLF = new byte[] { 13, 10 };
+    private static final byte CR = (byte)0x0D;
+    private static final byte LF = (byte)0x0A;
+    private static final byte[] CRLF = new byte[] { CR, LF };
+    private static String strCRLF = "\r\n";
 
     private UtilFactory utilFactory;
     protected String serverName;
@@ -127,22 +90,18 @@ public abstract class Connection {
     private String localAddress;
     private String connectionUrl;
     protected GlobalConfig globalConfig;
-    protected InputStream input;
-    protected OutputStream output;
     protected boolean useWiFi;
+    private InputStream input;
+    private OutputStream output;
     private int fakeAvailable = -1;
     private int bytesSent = 0;
     private int bytesReceived = 0;
-
+    
     /**
-     * Provides a buffer used for incoming data.
+     * Byte stream used to hold received data before it is passed back to
+     * the rest of the application.
      */
-    private byte[] buffer = new byte[128];
-
-    /**
-     * Provides a dynamic buffer for building results
-     */
-    DataBuffer resultBuffer = new DataBuffer();
+    private final ByteArrayOutputStream byteStream = new NoCopyByteArrayOutputStream(1024);
 
     /**
      * Initializes a new connection object.
@@ -223,8 +182,8 @@ public abstract class Connection {
 
         if (EventLogger.getMinimumLevel() >= EventLogger.INFORMATION) {
             String msg = "Connection established:\r\n" + "Socket: " +
-            socket.getClass().toString() + "\r\n" + "Local address: " +
-            localAddress + "\r\n";
+            socket.getClass().toString() + strCRLF + "Local address: " +
+            localAddress + strCRLF;
             EventLogger.logEvent(AppInfo.GUID, msg.getBytes(),
                     EventLogger.INFORMATION);
         }
@@ -357,77 +316,6 @@ public abstract class Connection {
     }
 
     /**
-     * Sends a string to the server. This method is used internally for
-     * all outgoing communication to the server. The main thing it does
-     * it terminate the line with a CR/LF. If there are occurrences of CR or
-     * LF inside the string, the method ensures that proper CR/LF sequences
-     * are sent for them, since this is what most Internet protocols expect.
-     *
-     * @see #receive
-     */
-    public synchronized void send(String s) throws IOException {
-        byte[] bytes = s.getBytes();
-        int length = bytes.length;
-
-        /**
-         * Special case for empty strings: Only CR/LF is sent.
-         */
-        if (s.length() == 0) {
-            if (globalConfig.getConnDebug()) {
-                EventLogger.logEvent(AppInfo.GUID, "[SEND]".getBytes(),
-                        EventLogger.DEBUG_INFO);
-            }
-
-            output.write(CRLF, 0, 2);
-            bytesSent += 2;
-        }
-        /**
-         * The usual case goes here.
-         */
-        else {
-            int i = 0;
-
-            while (i < length) {
-                int j = i;
-
-                /**
-                 * Find next occurrence of a line separator or the end of the
-                 * string.
-                 */
-                while ((j < length) && (bytes[j] != 0x0A) &&
-                        (bytes[j] != 0x0D)) {
-                    j++;
-                }
-
-                if (globalConfig.getConnDebug()) {
-                    EventLogger.logEvent(AppInfo.GUID,
-                            ("[SEND] " + s.substring(i, j)).getBytes(),
-                            EventLogger.DEBUG_INFO);
-                }
-
-                /**
-                 * Write the string up to there and terminate it properly.
-                 */
-                byte[] buf = (s.substring(i, j) + "\r\n").getBytes();
-                output.write(buf);
-                bytesSent += buf.length;
-
-                /**
-                 * If we stopped at a CR, ignore a possibly following LF.
-                 */
-                if ((j < (length - 1)) && (bytes[j] == 0x0D) &&
-                        (bytes[j + 1] == 0x0A)) {
-                    j++;
-                }
-
-                i = j + 1;
-            }
-        }
-
-        output.flush();
-    }
-
-    /**
      * Sends a string to the server, terminating it with a CRLF.
      * No cleanup is performed, as it is expected that the string
      * is a prepared protocol command.
@@ -442,7 +330,7 @@ public abstract class Connection {
             output.write(CRLF, 0, 2);
             bytesSent += 2;
         } else {
-            byte[] buf = (s + "\r\n").getBytes();
+            byte[] buf = (s + strCRLF).getBytes();
             output.write(buf);
             bytesSent += buf.length;
         }
@@ -485,152 +373,107 @@ public abstract class Connection {
             return fakeAvailable;
         }
     }
-
+    
     /**
      * Receives a string from the server. This method is used internally for
      * incoming communication from the server. The main thing it does is
-     * ensuring that only complete lines are returned to the application, that is,
-     * lines that were terminated at least by a CR. LFs are ignored completely.
-     * Neither CRs nor LFs are returned as part of the result.
+     * ensure that only complete lines are returned to the application, that is,
+     * lines that were terminated at least by a LF.  Neither CRs nor LFs are
+     * returned as part of the result.
      *
      * @see #send
      */
     public synchronized String receive() throws IOException {
-        /**
-         * A note on how this method works and why it is designed the
-         * way it is: A previous implementation tried to read multiple
-         * bytes from the InputStream, searched a possible line terminator
-         * in them, and appended characters to a temporary result string.
-         * That didn't work very well, because the code had to deal
-         * with a large number of temporary strings that grew as more
-         * characters belonging to a line were read. The result was
-         * a high level of heap fragmentation, which made applications
-         * unstable on MIDP devices (that don't provide compacting GC,
-         * that is).
-         *
-         * This new implementation seems to work better: Is reads
-         * characters from the InputStream one-by-one, putting them
-         * in a buffer of fixed size that is large enough to hold a
-         * "normal" line belonging to an e-mail. When a terminator
-         * is found, a new String object is created from that buffer.
-         * Since the buffer is of fixed size and used during the whole
-         * lifetime of the Connection object, and the String is the
-         * actual String that makes it into the final Message object,
-         * there are no temporary Strings needed -- at least not in the
-         * common case: If a mail server tends to send whole messages
-         * without line terminators (and thus disregards for example
-         * the POP3 specification that dictates a maximum response of
-         * 512 characters), temporary Strings will again be necessary,
-         * but there's not much one can about that.
-         */
-        boolean stop = false;
-        resultBuffer.reset();
+        // Check existing data for a usable line
+        String line = checkForLine();
+        if(line != null) {
+            return line;
+        }
 
-        int actualAvailable = input.available();
-        int readBytes = 0;
-        int count;
-
-        /**
-         * The "stop" flag will be set as soon as we have received
-         * a complete line, that is, a line terminated by CR/LF. Until
-         * this is the case, the following block is repeated.
-         */
-        while (!stop) {
-            count = 0;
-
-            /**
-             * The inner block reads single bytes from the InputStream
-             * until, again, a line terminator is read or the buffer is
-             * full.
-             */
-            while (true) {
-                int actual = input.read(buffer, count, 1);
-
-                /**
-                 * If -1 is returned, the InputStream is already closed,
-                 * probably because the connection is broken, or the server
-                 * didn't like is. Try to close the connection, but ignore
-                 * any errors that might result from this.
-                 */
-                if (actual == -1) {
-                    EventLogger.logEvent(AppInfo.GUID,
-                            "Unable to read from socket, closing connection".getBytes(),
-                            EventLogger.INFORMATION);
-
-                    try {
-                        close();
-                    } catch (IOException e) {
-                    }
-
-                    throw new IOException("Connection closed");
+        // Read from the socket
+        int firstByte = input.read();
+        if(firstByte != -1) {
+            byteStream.write((byte)firstByte);
+            bytesReceived++;
+            int bytesAvailable = input.available();
+            while(bytesAvailable > 0) {
+                byte[] buf = new byte[bytesAvailable];
+                input.read(buf);
+                byteStream.write(buf);
+                bytesReceived += bytesAvailable;
+                
+                // Check read data for a usable line
+                line = checkForLine();
+                if(line != null) {
+                    return line;
                 }
-                /**
-                 * If no bytes have been received, we wait a little
-                 * while (by yielding processing time to other threads).
-                 */
-                else if (actual == 0) {
-                    try {
-                        Thread.yield();
-                    } catch (Exception e) {
-                    }
-                }
-                /**
-                 * If a byte has been read, examine it and put it in the
-                 * buffer.
-                 */
-
-                // Note: We really should look for CRLF, and not use this
-                // approach which screws up on mid-line LFs. (DK)
-                else {
-                    bytesReceived += actual;
-
-                    byte b = buffer[count];
-                    readBytes++;
-
-                    /**
-                     * Ignore all CRs.
-                     */
-                    if (b == 0x0D) {
-                        /* Ignore CRs */
-                    }
-                    /**
-                     * Take LF as line separator.
-                     */
-                    else if (b == 0x0A) {
-                        stop = true;
-
-                        break;
-                    }
-                    /**
-                     * Everything else makes it into the buffer.
-                     */
-                    else {
-                        count++;
-
-                        if (count == buffer.length) {
-                            break;
-                        }
-                    }
-                }
+                
+                bytesAvailable = input.available();
             }
-
-            resultBuffer.write(buffer, 0, count);
         }
-
-        String result = new String(resultBuffer.toArray());
-
-        if (globalConfig.getConnDebug()) {
+        else {
+            // If we got here, that means that the InputStream is either closed
+            // or we are in some otherwise unrecoverable state.  This means we
+            // will try to close the connection, ignore any errors from the
+            // close operation, and throw an IOException.
+            
             EventLogger.logEvent(AppInfo.GUID,
-                    ("[RECV] " + result).getBytes(),
-                    EventLogger.DEBUG_INFO);
-        }
+                    "Unable to read from socket, closing connection".getBytes(),
+                    EventLogger.INFORMATION);
 
-        if (actualAvailable > readBytes) {
-            fakeAvailable = actualAvailable - readBytes;
-        } else {
-            fakeAvailable = -1;
-        }
+            try {
+                close();
+            } catch (IOException e) { }
 
+            throw new IOException("Connection closed");
+        }
+        
+        // This should never normally happen
+        return null;
+    }
+    
+    /**
+     * Checks the byte stream buffer for a usable line of returnable data.
+     * If a line is returned, the buffer will be updated to only contain data
+     * following that line.
+     *
+     * @return the trimmed string which ended in a CRLF in the source data
+     */
+    private String checkForLine() throws IOException {
+        String result;
+        
+        byte[] buf = byteStream.toByteArray();
+        int size = byteStream.size();
+        
+        int p = Arrays.getIndex(buf, LF);
+        if(p != -1 && p < size) {
+            int q = p + 1;
+            if(p > 0 && buf[p - 1] == CR) {
+                p--;
+            }
+            result = new String(buf, 0, p);
+            
+            if (globalConfig.getConnDebug()) {
+                EventLogger.logEvent(AppInfo.GUID,
+                        ("[RECV] " + result).getBytes(),
+                        EventLogger.DEBUG_INFO);
+            }
+            
+            if(q < size) {
+                buf = Arrays.copy(buf, q, size - q);
+                byteStream.reset();
+                byteStream.write(buf);
+                fakeAvailable = buf.length;
+            }
+            else {
+                byteStream.reset();
+                fakeAvailable = -1;
+            }
+        }
+        else {
+            fakeAvailable = size;
+            result = null;
+        }
         return result;
     }
 
